ece34dfde98fa79e10e0ed6f29ca6a83ef937986
gongDongXiang <55584419+gongDongXiang@users.noreply.github.com>
2019-12-03 13:25:20 +0800

    Update Bird.java

--- src/com/xbj/domain/Bird.java
+++ src/com/xbj/domain/Bird.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import java.io.Serializable;
 @Data
 public class Bird implements Serializable {
+    // 实体类
     private Integer birdId;
     private String birdName;
     private String password;
@@ -12,5 +13,6 @@ public class Bird implements Serializable {
     private String hobbies;
     private String address;
     private String email;
+    
 
 }
